cca700d1e148a2e75c87d39e4caa8142ee1be841
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-04-16 18:47:47 +0530

    Right View of Binary Tree

---
+++ Lecture 64/Program 189.cpp
@@ -0,0 +1,69 @@
+// Right View of Binary Tree
+//  https://www.geeksforgeeks.org/problems/right-view-of-binary-tree/1
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+};
+// Utility function to create a new Tree Node
+Node *newNode(int val)
+{
+    Node *temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+// } Driver Code Ends
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+// Should return  right view of tree
+class Solution
+{
+public:
+    // Function to return list containing elements of right view of binary tree.
+    void solve(Node *root, vector<int> &ans, int level)
+    {
+        // Base case
+        if (root == NULL)
+        {
+            return;
+        }
+
+        // We entered into a new level
+        if (level == ans.size())
+        {
+            ans.push_back(root->data);
+        }
+
+        solve(root->right, ans, level + 1);
+        solve(root->left, ans, level + 1);
+    }
+    vector<int> rightView(Node *root)
+    {
+        vector<int> ans;
+        solve(root, ans, 0);
+        return ans;
+    }
+};
