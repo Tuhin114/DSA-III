7a1c011f55b50da87edc1a68871ce9c7cb322d36
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-04-23 18:07:38 +0530

    Deletion, max , min value in BST

--- Lecture 69/Program 199.cpp
+++ Lecture 69/Program 199.cpp
@@ -117,6 +117,88 @@ Node *insertIntoBST(Node *root, int d)
     return root;
 }
 
+Node *minVal(Node *root)
+{
+    Node *temp = root;
+
+    while (temp->left != NULL)
+    {
+        temp = temp->left;
+    }
+
+    return temp;
+}
+
+Node *maxVal(Node *root)
+{
+    Node *temp = root;
+
+    while (temp->right != NULL)
+    {
+        temp = temp->right;
+    }
+
+    return temp;
+}
+
+Node *deleteFromBST(Node *root, int val)
+{
+    // Base case
+    if (root == NULL)
+    {
+        return root;
+    }
+
+    if (root->data == val)
+    {
+        // 0 child
+        if (root->left == NULL && root->right == NULL)
+        {
+            delete root;
+            return NULL;
+        }
+
+        // 1 child
+
+        // Left child
+        if (root->left != NULL && root->right == NULL)
+        {
+            Node *temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        // Right child
+        if (root->left == NULL && root->right != NULL)
+        {
+            Node *temp = root->right;
+            delete root;
+            return temp;
+        }
+
+        // 2 child
+        if (root->left != NULL && root->right != NULL)
+        {
+            int mini = minVal(root->right)->data;
+            root->data = mini;
+            root->right = deleteFromBST(root->right, mini);
+            return root;
+        }
+    }
+    else if (root->data > val)
+    {
+        // Go to left part
+        root->left = deleteFromBST(root->left, val);
+        return root;
+    }
+    else
+    {
+        // Go to right part
+        root->right = deleteFromBST(root->right, val);
+        return root;
+    }
+}
+
 void takeInput(Node *&root)
 {
     int data;
@@ -149,6 +231,30 @@ int main()
     cout << endl
          << "Printing the postorder BST" << endl;
     postorder(root);
+
+    cout << endl
+         << "Min value is " << minVal(root)->data << endl;
+    cout << "Max value is " << maxVal(root)->data << endl;
+
+    // Deletion
+    root = deleteFromBST(root, 90);
+    cout << "Printing the BST" << endl;
+    levelOrderTraversal(root);
+
+    cout << "Printing the inorder BST" << endl;
+    inorder(root);
+
+    cout << endl
+         << "Printing the preorder BST" << endl;
+    preorder(root);
+
+    cout << endl
+         << "Printing the postorder BST" << endl;
+    postorder(root);
+
+    cout << endl
+         << "Min value is " << minVal(root)->data << endl;
+    cout << "Max value is " << maxVal(root)->data << endl;
 
     return 0;
 }
