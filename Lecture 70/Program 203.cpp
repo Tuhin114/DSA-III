618bf0d39cd237e228bd3cf58400ce3c3d30e9da
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-04-24 18:34:36 +0530

    CodeStudio - Predecessor And Successor In BST

---
+++ Lecture 70/Program 203.cpp
@@ -0,0 +1,44 @@
+// CodeStudio - Predecessor And Successor In BST
+// https://www.naukri.com/code360/problems/_893049?topList=love-babbar-dsa-sheet-problems&leftPanelTab=0&leftPanelTabValue=PROBLEM
+
+/**
+ pair<int, int> predecessorSuccessor(TreeNode *root, int key)
+{
+    TreeNode* temp = root;
+    int pred = -1;
+    int succ = -1;
+
+    while(temp && temp->data!=key){
+
+        if(temp->data > key){
+            succ = temp -> data;
+            temp = temp -> left;
+        }
+        else{
+            pred = temp -> data;
+            temp = temp -> right;
+        }
+    }
+
+    if(temp){
+
+        TreeNode* leftTree = temp -> left;
+
+        while(leftTree!=NULL){
+            pred = leftTree->data;
+            leftTree = leftTree -> right;
+        }
+
+    TreeNode* rightTree = temp -> right;
+
+        while(rightTree!=NULL){
+            succ = rightTree->data;
+            rightTree = rightTree -> left;
+        }
+    }
+
+    pair<int,int> ans = make_pair(pred,succ);
+    return ans;
+
+}
+*/
